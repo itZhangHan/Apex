b20c33c227fea23c1f0e22db6ee95f84ac6e4f5d
zhanghan <13146353585@163.com>
2017-10-12 17:22:39 +0800

    10.22 17:22 修改。更新项目

--- src/main/java/com/els/controller/WebSocketController.java
+++ src/main/java/com/els/controller/WebSocketController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebSocketController {
 
-	@RequestMapping("/index")
+	@RequestMapping("/index1")
 	public String toIndex() {
 
 		return "index";
