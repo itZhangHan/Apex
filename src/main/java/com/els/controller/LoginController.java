dbe616084057808a0ac695c13f4c7158530e0d58
zhanghan <13146353585@163.com>
2017-10-18 16:09:19 +0800

    微信授权 测试2  10.18 16:09

--- src/main/java/com/els/controller/LoginController.java
+++ src/main/java/com/els/controller/LoginController.java
@@ -5,18 +5,19 @@ import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.http.HttpResponse;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.els.common.AuthUtil;
 /*
  * 授权登录
  */
+@Controller
 @RequestMapping("/WxAuth")
 public class LoginController {
 
 	@RequestMapping("/wxLogin")
-	public void wxAuthorze(HttpResponse resp) throws IOException{
+	public void wxAuthorze(HttpServletResponse resp) throws IOException{
 		System.out.printf("进入登录方法");
 
 		// String backUrl = "http://atwjsw.s1.natapp.cc/WxAuth/callBack";
@@ -26,7 +27,7 @@ public class LoginController {
 				+ "&redirect_uri=" + URLEncoder.encode(backUrl) + "&response_type=code" + "&scope=snsapi_userinfo"
 				+ "&state=STATE" + "#wechat_redirect";
 		// 重定向用户请求到微信授权URL
-		((HttpServletResponse) resp).sendRedirect(url);
+		resp.sendRedirect(url);
 		// super.doGet(req, resp);
 	}
 }
