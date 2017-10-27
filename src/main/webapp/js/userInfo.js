3961c24258899ecd36c52a36de3b8843fd900665
zhanghan <13146353585@163.com>
2017-10-27 15:00:34 +0800

    15:00

---
+++ src/main/webapp/js/userInfo.js
@@ -0,0 +1,35 @@
+var request = {
+	QueryString : function(val) {
+		var uri = window.location.search;
+		var re = new RegExp("" + val + "\=([^\&\?]*)", "ig");
+		return ((uri.match(re)) ? (uri.match(re)[0].substr(val.length + 1))
+				: null);
+	},
+	QueryStrings : function() {
+		var uri = window.location.search;
+		var re = /\w*\=([^\&\?]*)/ig;
+		var retval = [];
+		while ((arr = re.exec(uri)) != null)
+			retval.push(arr[0]);
+		return retval;
+	},
+	setQuery : function(val1, val2) {
+		var a = this.QueryStrings();
+		var retval = "";
+		var seted = false;
+		var re = new RegExp("^" + val1 + "\=([^\&\?]*)$", "ig");
+		for (var i = 0; i < a.length; i++) {
+			if (re.test(a[i])) {
+				seted = true;
+				a[i] = val1 + "=" + val2;
+			}
+		}
+		retval = a.join("&");
+		return "?" + retval
+				+ (seted ? "" : (retval ? "&" : "") + val1 + "=" + val2);
+	}
+}
+nicakname = decodeURI(request.QueryString("nickname"));
+city = decodeURI(request.QueryString("city"));
+img = decodeURI(request.QueryString("headimgurl"));
+sex = decodeURI(request.QueryString("sex"));
