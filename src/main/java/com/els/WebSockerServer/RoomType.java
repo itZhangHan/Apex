8235e4774d74a19dc1c49afe6a8272e7998953b5
zhanghan <13146353585@163.com>
2017-10-31 14:12:38 +0800

    14:12

--- src/main/java/com/els/WebSockerServer/RoomType.java
+++ src/main/java/com/els/WebSockerServer/RoomType.java
@@ -1,8 +1,16 @@
 package com.els.WebSockerServer;
 
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import javax.websocket.EncodeException;
+
+import com.els.socket.SocketManger;
 import com.els.socket.SocketMessage;
+
 /**
  * 房间状态 0未开始 1：游戏中 2:游戏结束
+ * 
  * @author Administrator
  *
  */
@@ -10,7 +18,29 @@ public class RoomType implements InterfaceType {
 
 	@Override
 	public String onMessage(SocketMessage message) {
-		// TODO Auto-generated method stub
+		// 如果返回信息包含roomid
+		CopyOnWriteArraySet<WebSocketServer> arrayset = SocketManger.getRoomArray(message.getRoomId());
+		if (arrayset != null) {
+			for (WebSocketServer object : arrayset) {
+				try {
+					try {
+						System.out.println("sendmessage...roomStatus");
+						if (message.getRoomState().equals("0")) {
+							message.setRoomState("1");
+							message.setMsgStr("游戏开始了");
+							object.getSession().getBasicRemote().sendObject(message);
+						}
+					} catch (EncodeException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					// object.session.getAsyncRemote()).sendMessage(message.getMsgStr());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 		return null;
 	}
 
