5ee056a45a9de6f999ee543bc744d55f5ed6809d
zhanghan <13146353585@163.com>
2017-11-07 14:16:34 +0800

    14:16

--- src/main/webapp/js/webSocket.js
+++ src/main/webapp/js/webSocket.js
@@ -4,7 +4,6 @@
 
 var websocket = new WebSocket("ws://thdd.apexgame.cn/tetris/websocket1");
 websocket.onOpen = function() {
-	
 
 }
 
@@ -12,5 +11,23 @@ websocket.onClose = function() {
 
 }
 
-
 this.websocket = websocket;
+function userMessage() {
+
+	var userMessage = {};
+	userMessage.type = "usmesage"
+	userMessage.roomId = roomId;
+	userMessage.toUserName = "";
+	userMessage.fromUserName = "";
+	userMessage.msgStr = "";
+	userMessage.userStatus = userStatus;
+	userMessage.roomState = roomState;
+	userMessage.position = "1";
+	userMessage.headimgurl = img;
+	userMessage.nicakname = nicakname;
+	userMessage.sex = sex;
+	userMessage.city = city;
+	userMessage.userId = userId;
+	alert(city);
+	websocket.send(JSON.stringify(userMessage));
+}
