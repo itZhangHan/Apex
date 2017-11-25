d7b1a858ff57da1ab66aa13b56d5014910aa307d
zhanghan <13146353585@163.com>
2017-11-25 18:34:04 +0800

    18:33

---
+++ src/main/java/com/els/socket/Readers.java
@@ -0,0 +1,95 @@
+package com.els.socket;
+
+public class Readers {
+
+	private String position1;
+
+	private String position2;
+
+	private String position3;
+
+	private String position4;
+
+	private String headImg1;
+	
+	private String headImg2;
+	
+	private String headImg3;
+	
+	private String headImg4;
+	
+	private Integer[] a;
+
+	public Integer[] getA() {
+		return a;
+	}
+
+	public void setA(Integer[] a) {
+		this.a = a;
+	}
+
+	public String getHeadImg1() {
+		return headImg1;
+	}
+
+	public void setHeadImg1(String headImg1) {
+		this.headImg1 = headImg1;
+	}
+
+	public String getHeadImg2() {
+		return headImg2;
+	}
+
+	public void setHeadImg2(String headImg2) {
+		this.headImg2 = headImg2;
+	}
+
+	public String getHeadImg3() {
+		return headImg3;
+	}
+
+	public void setHeadImg3(String headImg3) {
+		this.headImg3 = headImg3;
+	}
+
+	public String getHeadImg4() {
+		return headImg4;
+	}
+
+	public void setHeadImg4(String headImg4) {
+		this.headImg4 = headImg4;
+	}
+
+	public String getPosition1() {
+		return position1;
+	}
+
+	public void setPosition1(String position1) {
+		this.position1 = position1;
+	}
+
+	public String getPosition2() {
+		return position2;
+	}
+
+	public void setPosition2(String position2) {
+		this.position2 = position2;
+	}
+
+	public String getPosition3() {
+		return position3;
+	}
+
+	public void setPosition3(String position3) {
+		this.position3 = position3;
+	}
+
+	public String getPosition4() {
+		return position4;
+	}
+
+	public void setPosition4(String position4) {
+		this.position4 = position4;
+	}
+
+}
