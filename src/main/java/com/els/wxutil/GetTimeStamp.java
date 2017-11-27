140093af39737a107461d85bd4fbafc2ce53f3bc
zhanghan <13146353585@163.com>
2017-11-27 13:56:43 +0800

    微信自定义 13:56

---
+++ src/main/java/com/els/wxutil/GetTimeStamp.java
@@ -0,0 +1,10 @@
+package com.els.wxutil;
+
+import com.sun.tools.javac.util.Convert;
+
+public class GetTimeStamp {
+/*	public string getTimestamp() {
+		TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+		return Convert.ToInt64(ts.TotalSeconds).ToString();
+	}*/
+}
---
+++ src/main/java/com/els/wxutil/MyX509TrustManager.java
@@ -0,0 +1,7 @@
+package com.els.wxutil;
+
+import javax.net.ssl.TrustManager;
+
+public class MyX509TrustManager implements TrustManager {
+
+}
