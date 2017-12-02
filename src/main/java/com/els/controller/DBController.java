bd7d5c18b698aedf0623ab1dc1a934fc6e2037ad
zhanghan <13146353585@163.com>
2017-12-02 14:05:34 +0800

    14.05

--- src/main/java/com/els/controller/DBController.java
+++ src/main/java/com/els/controller/DBController.java
@@ -1,9 +1,31 @@
 package com.els.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.els.bean.JhddPositionimg;
+import com.els.service.PositionImgServiceImpl;
+import com.els.serviceinterface.PositionImgService;
 
 @Controller
 public class DBController {
 
+	@Autowired
+	private static PositionImgServiceImpl positionImgService;
+	
+	public PositionImgServiceImpl getPositionImgService() {
+		return positionImgService;
+	}
+
+	public void setPositionImgService(PositionImgServiceImpl positionImgService) {
+		this.positionImgService = positionImgService;
+	}
+
+	@RequestMapping
+	public void addPositionImg(JhddPositionimg positionImg){
+		
+		
+	}
 	
 }
